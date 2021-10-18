5dd195d3e0d9f538d227954a51d93e752413c9cd
Rishab Banerjee <crpbanerjee@gmail.com>
2021-10-18 02:09:52 -0700

    Word Break
    139. Word Break
    Forgot to add the code in the last commit.

--- 139.java
+++ 139.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    private boolean wordBreak(String s, Set<String> wordDictSet, Map<String, Boolean> dp) {
+        if (s.length() == 0 || wordDictSet.contains(s)) {
+            return true;
+        }
+        
+        for (int i = 1; i < s.length(); ++i) {
+            String stringPrefix = s.substring(0, i);
+            String stringSuffix = s.substring(i);
+            if (!dp.containsKey(stringSuffix)) {
+                dp.put(stringSuffix, wordBreak(stringSuffix, wordDictSet, dp));
+            }
+            // System.out.println(stringPrefix + " " + stringSuffix);
+            if (wordDictSet.contains(stringPrefix) && dp.get(stringSuffix)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        Map<String, Boolean> dp = new HashMap<>();
+        return wordBreak(s, wordDictSet, dp);
+    }
+}
