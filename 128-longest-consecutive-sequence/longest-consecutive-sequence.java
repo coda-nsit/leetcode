507c32280785a6176aee2434d220612783f6147c
Rishab Banerjee <crpbanerjee@gmail.com>
2026-03-28 16:58:14 -0700

    Time: 51 ms (9.39%) | Memory: 103.6 MB (5.02%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -0,0 +1,23 @@
+/**
+For any consecutive sequence, we just care about the first element of the sequence. So, of we see num and num - 1 exists, then we don't care about num.
+As soon as we encounter the first element of a sequence i.e. num - 1 doesn't exist, we calculate how long is the sequence that starts with num.
+ */
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toSet()));
+        int maxi, currSeqLength;
+
+        maxi = 0;
+        for (int num: numsSet) {
+            // new sequence can start
+            if (!numsSet.contains(num - 1)) {
+                currSeqLength = 1;
+                while (numsSet.contains(num + currSeqLength)) {
+                    currSeqLength += 1;
+                }
+                maxi = Math.max(maxi, currSeqLength);
+            }
+        }
+        return maxi;
+    }
+}
