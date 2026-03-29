8acd1912cd80aaac744954e6542bfebb2895e2b5
Rishab Banerjee <crpbanerjee@gmail.com>
2026-03-28 17:01:04 -0700

    Time: 67 ms (5.91%) | Memory: 127.3 MB (5.02%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -5,7 +5,7 @@ As soon as we encounter the first element of a sequence i.e. num - 1 doesn't exi
 class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> numsSet = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toSet()));
-        List<Integer> numsNoDups = new ArrayList<>(Arrays.stream(nums).boxed().collect(Collectors.toSet()));
+        List<Integer> numsNoDups = new ArrayList<>(Arrays.stream(nums).boxed().collect(Collectors.toSet())); // optimization in case there are duplicates. No part of core algorithm.
         int maxi, currSeqLength;
 
         maxi = 0;
